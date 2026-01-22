e7aeee6cfa65d700b2e591fba215009bda5ad6a3
EdS <Sousaa.dev@gmail.com.br>
2026-01-22 16:06:23 -0300

    Migrando projeto Cadastro de usuarios para outro repositorio no GitHub

---
+++ src/main/java/com/eduardo/usuarios/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.eduardo.usuarios.infrastructure.repository;
+
+import com.eduardo.usuarios.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/eduardo/usuarios/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.eduardo.usuarios.infrastructure.repository;
+
+import com.eduardo.usuarios.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
